866cb719e2af21b659c28c3797e98bb404cea908
KyleWashington73 <91575541+KyleWashington73@users.noreply.github.com>
2022-06-15 09:07:29 -0700

    ok

---
+++ front-page.php
@@ -0,0 +1,18 @@
+<?php 
+/** 
+ * Template Name: Homepage 
+ * 
+ * @package Children of Incarcerated Parents
+ * 
+ */ 
+?> 
+<?php get_header(); ?>
+
+ <!-- This is the start of hero section -->
+ <main id="topBtn">
+        <div class="hero-text">
+            <h1>Children of Incarcerated Parents</h1>
+            <p></p>
+        </div>
+    </main>
+    <!-- This is the end of hero section --> 
